e324bbc01846f52a5859857c02dd6a5f000bc8c6
MinHyuk Kim <rhrlwld@gmail.com>
2022-07-21 15:07:14 +0900

    crud 적용

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -51,6 +51,29 @@ function Create(props){
   </article>
 }
 
+// onChange : html에서는 값이 바뀌고 마우스포인터가 밖으로 빠져나갈때 동작하지만 리액트에서는 값을 입력할 때마다 onChange가 호출됨
+function Update(props){
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return <article>
+    <h2>Update</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onUpdate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title" value={title} onChange={event=>{
+        setTitle(event.target.value);
+      }}/></p> 
+      <p><textarea name="body" placeholder='body' value={body} onChange={event=>{
+        setBody(event.target.value);
+      }}></textarea></p>
+      <p><input type="submit" value="Update" /></p>
+    </form>
+  </article>
+}
+
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
@@ -62,20 +85,47 @@ function App() {
   ] );
 
   let content = null;
+  let contextControl = null;
 
-  if(mode === 'WELCOME'){
+  if(mode === 'WELCOME')
+  {
     content = <Article title="Welcome" body="Hello, WEB"></Article>
-  } else if(mode === 'READ'){
+  } 
+  else if(mode === 'READ')
+  {
     let title, body = null;
-    for(let i = 0; i<topics.length; i++){
-      console.log(topics[i].id, id);
-      if(topics[i].id === id){
+    for(let i = 0; i<topics.length; i++)
+    {
+      if(topics[i].id === id)
+      {
         title = topics[i].title;
         body = topics[i].body;
       }
     }
+
     content = <Article title={title} body={body}></Article>
-  } else if(mode === 'CREATE'){
+    contextControl = <><li><a href={'/update/'+id} onClick={event=>{
+      event.preventDefault();
+      setMode('UPDATE');
+    }}>Update
+    </a>
+  </li>
+  <li>
+    <input type="button" value="Delete" onClick={()=>{
+      const newTopics = [];
+      for(let i=0; i<topics.length; i++){
+        if(topics[i].id !== id){
+          newTopics.push(topics[i]);
+        }
+      }
+      setTopics(newTopics);
+      setMode('WELCOME');
+    }}/>
+  </li>
+  </>
+  } 
+  else if(mode === 'CREATE')
+  {
     content = <Create onCreate={(_title, _body)=>{
       const newTopic = {id:nextId, title:_title, body:_body}
       const newTopics = [...topics] // JS 불변성 개념 : useState(객체)의 경우, 복제본을 만들어서 그곳에 push하고 사용한다 (객체 : array, object 등등)
@@ -85,9 +135,36 @@ function App() {
       setId(nextId);
       setNextId(nextId+1);
     }}>
-
     </Create>
   } 
+
+  else if(mode === 'UPDATE')
+  {
+    let title, body = null;
+    for(let i = 0; i<topics.length; i++)
+    {
+      if(topics[i].id === id)
+      {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+
+    content = <Update title={title} body={body} onUpdate={(title, body)=>{
+      console.log(title, body);
+      const updatedTopic = {id:id, title:title, body:body}
+      const newTopics = [...topics]
+      for(let i=0; i<newTopics.length; i++){
+        if(newTopics[i].id === id){
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode('READ');
+
+    }}></Update>
+  }
 
   return (
     <div className="App">
@@ -99,13 +176,17 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
-
-      <a href='/create' onClick={event=>{
-        event.preventDefault();
-        setMode('CREATE');
-      }}>
-        Create
-        </a>
+      <ul>
+      <li>
+          <a href='/create' onClick={event=>{
+          event.preventDefault();
+          setMode('CREATE');
+        }}>
+          Create
+          </a>
+        </li>
+        {contextControl}
+        </ul>
     </div>
   );
 }
